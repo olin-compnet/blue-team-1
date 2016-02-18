97b20e3ebcafbd5f38f0e393338c122d9d60429c
Danny Wolf <2dawolf@gmail.com>
2016-02-18 11:21:22 -0500

    Auto-deploy at 2016-02-18_11-21-22

--- transport/receiveblinks.py
+++ transport/receiveblinks.py
@@ -6,11 +6,11 @@ import time
 
 def receiveblinks(RXpin,blinks=200,duration=.25):
     for i in range(blinks):
-    	time.sleep(duration)
+        time.sleep(duration)
         print("{}".format("|" if RXpin.read_pin() else "."),end="\n")
 
 if __name__ == "__main__":   
             
     with SetPin(16,"GPIO_23",direction="RX") as RXpin:
-    	while True:
-        	receiveblinks(RXpin)
+        while True:
+            receiveblinks(RXpin)
